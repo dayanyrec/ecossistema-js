e3c6e8391c8f9629465fd7200c1d593b72d9ceec
Dayany Espindola <dayanyrec@gmail.com>
2018-06-09 01:53:42 -0300

    Usa grunt para concatenação

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,24 @@ module.exports = function(grunt) {
         src: 'index.js',
         dest: 'index.min.js'
       }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['node_modules/jquery/dist/jquery.min.js', 'index.min.js'],
+        dest: 'built.js',
+      },
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'concat']);
 
 };
